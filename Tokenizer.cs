19a6e699d443c5a9ecfa1187c36257145d299083
Andrei Diamandescu <diamandescuandrei@gmail.com>
2025-05-02 18:42:58 +0100

    Implement the tokenizer logic for input processing

---
+++ Tokenizer.cs
@@ -0,0 +1,107 @@
+﻿namespace LanguageInterpreter
+{
+    public class Tokenizer
+    {
+        private readonly string _source;
+        private readonly List<Token> _tokens = new List<Token>();
+
+        private int _start = 0;
+        private int _current = 0;
+        private int _line = 1;
+
+        public Tokenizer(string source)
+        {
+            _source = source;
+        }
+
+        public List<Token> ScanTokens()
+        {
+            while (!IsAtEnd())
+            {
+                _start = _current;
+                ScanToken();
+            }
+
+            _tokens.Add(new Token(TokenType.EOF, "", null!, _line));
+            return _tokens;
+        }
+
+        private void ScanToken()
+        {
+            char c = Advance();
+            switch (c)
+            {
+                case '(': AddToken(TokenType.LEFT_PAREN); break;
+                case ')': AddToken(TokenType.RIGHT_PAREN); break;
+                case '-': AddToken(TokenType.MINUS); break;
+                case '+': AddToken(TokenType.PLUS); break;
+                case '*': AddToken(TokenType.STAR); break;
+                case '/': AddToken(TokenType.SLASH); break;
+
+                case ' ':
+                case '\r':
+                case '\t':
+                    break;
+
+                default:
+                    if (IsDigit(c))
+                    {
+                        Number();
+                    }
+                    else
+                    {
+                        throw new Exception($"Unexpected character: {c}");
+                    }
+                    break;
+            }
+        }
+
+        private void Number()
+        {
+            while (IsDigit(Peek())) Advance();
+
+            if (Peek() == '.' && IsDigit(PeekNext()))
+            {
+                Advance();
+
+                while (IsDigit(Peek())) Advance();
+            }
+
+            double value = double.Parse(_source.Substring(_start, _current - _start));
+            AddToken(TokenType.NUMBER, value);
+        }
+
+        private bool IsDigit(char c)
+        {
+            return c >= '0' && c <= '9';
+        }
+
+        private bool IsAtEnd()
+        {
+            return _current >= _source.Length;
+        }
+
+        private char Advance()
+        {
+            return _source[_current++];
+        }
+
+        private char Peek()
+        {
+            if (IsAtEnd()) return '\0';
+            return _source[_current];
+        }
+
+        private char PeekNext()
+        {
+            if (_current + 1 >= _source.Length) return '\0';
+            return _source[_current + 1];
+        }
+
+        private void AddToken(TokenType type, object literal = null)
+        {
+            string text = _source.Substring(_start, _current - _start);
+            _tokens.Add(new Token(type, text, literal, _line));
+        }
+    }
+}
