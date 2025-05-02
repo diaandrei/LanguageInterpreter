6434ff720800a1b67f9d35d5c5db0177cca63c74
Andrei Diamandescu <diamandescuandrei@gmail.com>
2025-05-02 19:13:04 +0100

    Fixed parser to match new expr classes

---
+++ Parser.cs
@@ -0,0 +1,143 @@
+﻿namespace LanguageInterpreter
+{
+    public class Parser
+    {
+        private class ParseError : Exception { }
+
+        private readonly List<Token> _tokens;
+        private int _current = 0;
+
+        public Parser(List<Token> tokens)
+        {
+            _tokens = tokens;
+        }
+
+        public Expr Parse()
+        {
+            try
+            {
+                return Expression();
+            }
+            catch (ParseError)
+            {
+                return null!;
+            }
+        }
+
+        private Expr Expression()
+        {
+            return Term();
+        }
+
+        private Expr Term()
+        {
+            Expr expr = Factor();
+
+            while (Match(TokenType.PLUS, TokenType.MINUS))
+            {
+                Token op = Previous();
+                Expr right = Factor();
+                expr = new BinaryExpr(expr, op, right);
+            }
+
+            return expr;
+        }
+
+        private Expr Factor()
+        {
+            Expr expr = Unary();
+
+            while (Match(TokenType.STAR, TokenType.SLASH))
+            {
+                Token op = Previous();
+                Expr right = Unary();
+                expr = new BinaryExpr(expr, op, right);
+            }
+
+            return expr;
+        }
+
+        private Expr Unary()
+        {
+            if (Match(TokenType.MINUS))
+            {
+                Token op = Previous();
+                Expr right = Unary();
+                return new UnaryExpr(op, right);
+            }
+
+            return Primary();
+        }
+
+        private Expr Primary()
+        {
+            if (Match(TokenType.NUMBER))
+            {
+                return new LiteralExpr(Previous().Literal);
+            }
+
+            if (Match(TokenType.LEFT_PAREN))
+            {
+                Expr expr = Expression();
+                Consume(TokenType.RIGHT_PAREN, "Expect ')' after expression.");
+                return new GroupingExpr(expr);
+            }
+
+            throw Error(Peek(), "Expect expression.");
+        }
+
+        private bool Match(params TokenType[] types)
+        {
+            foreach (var type in types)
+            {
+                if (Check(type))
+                {
+                    Advance();
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private Token Consume(TokenType type, string message)
+        {
+            if (Check(type)) return Advance();
+
+            throw Error(Peek(), message);
+        }
+
+        private bool Check(TokenType type)
+        {
+            if (IsAtEnd()) return false;
+            return Peek().Type == type;
+        }
+
+        private Token Advance()
+        {
+            if (!IsAtEnd()) _current++;
+            return Previous();
+        }
+
+        private bool IsAtEnd()
+        {
+            return Peek().Type == TokenType.EOF;
+        }
+
+        private Token Peek()
+        {
+            return _tokens[_current];
+        }
+
+        private Token Previous()
+        {
+            return _tokens[_current - 1];
+        }
+
+        private ParseError Error(Token token, string message)
+        {
+            Console.Error.WriteLine($"Error at {token}: {message}");
+            return new ParseError();
+        }
+    }
+}
