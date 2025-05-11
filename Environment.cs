56748a764d1acdb329630ceca7e7c8a2a3100989
Andrei Diamandescu <diamandescuandrei@gmail.com>
2025-05-11 10:34:19 +0100

    Create a new class to handle storing variables

---
+++ Environment.cs
@@ -0,0 +1,31 @@
+﻿namespace LanguageInterpreter
+{
+    public class Environment
+    {
+        private Dictionary<string, object> _values = new Dictionary<string, object>();
+
+        public void Define(string name, object value)
+        {
+            _values[name] = value;
+        }
+
+        public object Get(string name)
+        {
+            if (_values.ContainsKey(name))
+                return _values[name];
+
+            throw new RuntimeException($"Undefined variable '{name}'.");
+        }
+
+        public void Assign(string name, object value)
+        {
+            if (_values.ContainsKey(name))
+            {
+                _values[name] = value;
+                return;
+            }
+
+            throw new RuntimeException($"Undefined variable '{name}'.");
+        }
+    }
+}
