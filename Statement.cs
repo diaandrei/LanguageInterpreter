eaeaebc1b1080514e4022e25adafb528b1c213e7
Andrei Diamandescu <diamandescuandrei@gmail.com>
2025-05-14 12:21:08 +0100

    Update the statement class to handle control flow statement types

--- Statement.cs
+++ Statement.cs
@@ -62,4 +62,82 @@
             environment.Define(Name.Lexeme, value);
         }
     }
+
+    public class BlockStatement : Statement
+    {
+        public List<Statement> Statements { get; }
+
+        public BlockStatement(List<Statement> statements)
+        {
+            Statements = statements;
+        }
+
+        public override void Execute(Environment environment)
+        {
+            foreach (var statement in Statements)
+            {
+                statement.Execute(environment);
+            }
+        }
+    }
+
+    public class IfStatement : Statement
+    {
+        public Expr Condition { get; }
+        public Statement ThenBranch { get; }
+        public Statement ElseBranch { get; }
+
+        public IfStatement(Expr condition, Statement thenBranch, Statement elseBranch)
+        {
+            Condition = condition;
+            ThenBranch = thenBranch;
+            ElseBranch = elseBranch;
+        }
+
+        public override void Execute(Environment environment)
+        {
+            if (IsTruthy(Condition.Evaluate(environment)))
+            {
+                ThenBranch.Execute(environment);
+            }
+            else if (ElseBranch != null)
+            {
+                ElseBranch.Execute(environment);
+            }
+        }
+
+        private bool IsTruthy(object obj)
+        {
+            if (obj == null) return false;
+            if (obj is bool) return (bool)obj;
+            return true;
+        }
+    }
+
+    public class WhileStatement : Statement
+    {
+        public Expr Condition { get; }
+        public Statement Body { get; }
+
+        public WhileStatement(Expr condition, Statement body)
+        {
+            Condition = condition;
+            Body = body;
+        }
+
+        public override void Execute(Environment environment)
+        {
+            while (IsTruthy(Condition.Evaluate(environment)))
+            {
+                Body.Execute(environment);
+            }
+        }
+
+        private bool IsTruthy(object obj)
+        {
+            if (obj == null) return false;
+            if (obj is bool) return (bool)obj;
+            return true;
+        }
+    }
 }
